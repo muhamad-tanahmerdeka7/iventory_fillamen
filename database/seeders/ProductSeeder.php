4822c8ffb1b09e118af042cd6f2d7db09cfb04f3
muhamad-tanahmerdeka7 <muhay990@gmail.com>
2026-07-19 20:02:53 +0700

    dark & tabel product

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -13,11 +12,62 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::create([
-            'product_order' => '1100016029', // Anda wajib mengisi ini karena di migration tidak nullable
-            'product_name'  => 'CHICKEN TERIYAKI 1KG',
-            'material'      => 'C-00191',
-            'uom'           => 'KG',
-        ]);
+        $products = [
+            [
+                'product_order' => '1100016029',
+                'product_name'  => 'CHICKEN TERIYAKI 1KG',
+                'material'      => 'C-00191',
+                'uom'           => 'KG',
+            ],
+            [
+                'product_order' => '1100016030',
+                'product_name'  => 'BEEF TERIYAKI 1KG',
+                'material'      => 'B-00245',
+                'uom'           => 'KG',
+            ],
+            [
+                'product_order' => '1100016031',
+                'product_name'  => 'CHICKEN YAKINIKU 1KG',
+                'material'      => 'C-00252',
+                'uom'           => 'KG',
+            ],
+            [
+                'product_order' => '1100016032',
+                'product_name'  => 'SAUCE TERIYAKI',
+                'material'      => 'S-00101',
+                'uom'           => 'PCS',
+            ],
+            [
+                'product_order' => '1100016033',
+                'product_name'  => 'CHICKEN KATSU',
+                'material'      => 'C-00194',
+                'uom'           => 'KG',
+            ],
+            [
+                'product_order' => '1100016034',
+                'product_name'  => 'SAUCE ERA',
+                'material'      => 'E-00040',
+                'uom'           => 'PACK',
+            ],
+            [
+                'product_order' => '1100016035',
+                'product_name'  => 'BASO UDANG',
+                'material'      => 'C-00195',
+                'uom'           => 'PACK',
+            ],
+             [
+                'product_order' => '1100016036',
+                'product_name'  => 'TORI NO TEBA',
+                'material'      => 'T-00257',
+                'uom'           => 'PACK',
+            ],
+        ];
+
+        // Mencegah data ganda berdasarkan product_order
+        Product::upsert(
+            $products,
+            ['product_order'], // kolom unik
+            ['product_name', 'material', 'uom'] // kolom yang di-update jika sudah ada
+        );
     }
 }
